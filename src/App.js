e754d86de12520cc2cac2ea520de85253801cc69
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-06-02 15:36:18 -0400

    Add Global Styles and Fonts in it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import "./styles.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3{
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 export default function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
